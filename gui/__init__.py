c88ec20ba04ac5e766c8c1fac19811edf7ae4b16
ahmetasm123 <125503966+ahmetasm123@users.noreply.github.com>
2025-08-08 17:14:36 +0300

    doc: improve calculation documentation

---
+++ gui/__init__.py
@@ -0,0 +1,6 @@
+"""Graphical user interface for the RC and RL calculator.
+
+Modules in this package provide the Tkinter based application that wraps the
+core calculation routines.  It handles user interaction and presents circuit
+results visually.
+"""
